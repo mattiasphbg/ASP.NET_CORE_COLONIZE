4735e25e116d6ef6243f59ceaa0b03d91b019968
WhiteTree <mattiasp@hotmail.com>
2020-01-24 14:04:44 +0100

    AddseedData

---
+++ Colonize.Website/Data/Migrations/20200124100614_AddVoyageShipDesition.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Colonize.Website.Data.Migrations
+{
+    public partial class AddVoyageShipDesition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destination",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Destription = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destination", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ship",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Destription = table.Column<string>(nullable: true),
+                    PassagerCapacity = table.Column<int>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ship", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voyage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DestinationId = table.Column<int>(nullable: true),
+                    ShipId = table.Column<int>(nullable: true),
+                    Departureat = table.Column<DateTime>(nullable: false),
+                    ArrivalAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voyage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Voyage_Destination_DestinationId",
+                        column: x => x.DestinationId,
+                        principalTable: "Destination",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Voyage_Ship_ShipId",
+                        column: x => x.ShipId,
+                        principalTable: "Ship",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voyage_DestinationId",
+                table: "Voyage",
+                column: "DestinationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voyage_ShipId",
+                table: "Voyage",
+                column: "ShipId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Voyage");
+
+            migrationBuilder.DropTable(
+                name: "Destination");
+
+            migrationBuilder.DropTable(
+                name: "Ship");
+        }
+    }
+}
---
+++ Colonize.Website/Data/Migrations/20200124124237_addVoyage.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Colonize.Website.Data.Migrations
+{
+    public partial class addVoyage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voyage_Destination_DestinationId",
+                table: "Voyage");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voyage_Ship_ShipId",
+                table: "Voyage");
+
+            migrationBuilder.DropColumn(
+                name: "Destription",
+                table: "Ship");
+
+            migrationBuilder.DropColumn(
+                name: "PassagerCapacity",
+                table: "Ship");
+
+            migrationBuilder.DropColumn(
+                name: "Destription",
+                table: "Destination");
+
+            migrationBuilder.RenameColumn(
+                name: "Departureat",
+                table: "Voyage",
+                newName: "DepartureAt");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShipId",
+                table: "Voyage",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DestinationId",
+                table: "Voyage",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Ship",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PassengerCapacity",
+                table: "Ship",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Destination",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Destination",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Lorem ipsum dolor", "https://via.placeholder.com/480x360.png?text=Moon", "Moon" },
+                    { 2, "Lorem ipsum dolor", "https://via.placeholder.com/480x360.png?text=Mars", "Mars" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Ship",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "PassengerCapacity" },
+                values: new object[,]
+                {
+                    { 1, "Lorem ipsum dolor", "https://via.placeholder.com/480x360.png?text=Moonshot", "Moonshot", 200 },
+                    { 2, "Lorem ipsum dolor", "https://via.placeholder.com/480x360.png?text=Mars+Explorer", "Mars Explorer", 2800 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Voyage",
+                columns: new[] { "Id", "ArrivalAt", "DepartureAt", "DestinationId", "ShipId" },
+                values: new object[] { 1, new DateTime(2024, 6, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 6, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Voyage",
+                columns: new[] { "Id", "ArrivalAt", "DepartureAt", "DestinationId", "ShipId" },
+                values: new object[] { 2, new DateTime(2029, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2028, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 2 });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voyage_Destination_DestinationId",
+                table: "Voyage",
+                column: "DestinationId",
+                principalTable: "Destination",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voyage_Ship_ShipId",
+                table: "Voyage",
+                column: "ShipId",
+                principalTable: "Ship",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voyage_Destination_DestinationId",
+                table: "Voyage");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voyage_Ship_ShipId",
+                table: "Voyage");
+
+            migrationBuilder.DeleteData(
+                table: "Voyage",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Voyage",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Destination",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Destination",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Ship",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Ship",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Ship");
+
+            migrationBuilder.DropColumn(
+                name: "PassengerCapacity",
+                table: "Ship");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Destination");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartureAt",
+                table: "Voyage",
+                newName: "Departureat");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShipId",
+                table: "Voyage",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DestinationId",
+                table: "Voyage",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Destription",
+                table: "Ship",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PassagerCapacity",
+                table: "Ship",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Destription",
+                table: "Destination",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voyage_Destination_DestinationId",
+                table: "Voyage",
+                column: "DestinationId",
+                principalTable: "Destination",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voyage_Ship_ShipId",
+                table: "Voyage",
+                column: "ShipId",
+                principalTable: "Ship",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
